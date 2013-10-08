6eb5ae69c111e40996d3f3561df328856034638b
Yoctopuce Sarl <info@yoctopuce.com>
2013-10-08 18:44:40 +0200

    Integrate Yoctctopuce library

---
+++ index.js
@@ -0,0 +1,35 @@
+/* wrap the browser-side yoctolib_js library for node.js
+
+   usage:
+
+       var yapi = require('yoctolib');
+
+       if (yapi.yRegisterHub('http://127.0.0.1:4444/') != yapi.YAPI_SUCCESS) ...
+
+   note: this could load the library asynchronously, but there's only 30 files to load...
+
+ */
+
+var fs  = require('fs')
+  , vm  = require('vm')
+  , xhr = require('xmlhttprequest')
+  ;
+
+
+var api = { window         : { XMLHttpRequest : xhr.XMLHttpRequest }
+          , XMLHttpRequest : xhr.XMLHttpRequest
+          , console        : console
+          , navigator      : {}
+          }
+  , includeSync = function(filename) {
+      vm.runInNewContext(fs.readFileSync(filename).toString(), api, filename);
+    }
+  , dir = __dirname + '/lib'
+  ,  files  = fs.readdirSync(dir)
+  , i = files.indexOf('yocto_api.js')
+  ;
+
+if (i !== -1) files.splice(i, 1);
+includeSync(dir + '/yocto_api.js');
+for (i = 0; i < files.length; i++) includeSync(dir + '/' + files[i]);
+module.exports = api;
